b7bbaceba12562a80abdaa44769e9abb66d90b45
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-04-03 22:03:27 +0300

    :

--- AgriculturePresentation/ViewComponents/_DashboardChartPartial.cs
+++ AgriculturePresentation/ViewComponents/_DashboardChartPartial.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccesLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgriculturePresentation.ViewComponents
 {
     public class _DashboardChartPartial : ViewComponent
     {
+        AgricultureContext context = new AgricultureContext();
+
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = 27;
+            ViewBag.v2 = 70;
+            ViewBag.v3 = 1;
+            ViewBag.v3 = 2;
             return View();
         }
     }
