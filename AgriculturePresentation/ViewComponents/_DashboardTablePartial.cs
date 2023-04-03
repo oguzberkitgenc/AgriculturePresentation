2fd36c24de64c70fa7886f632c7d4f669fce60ac
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-04-03 17:45:07 +0300

    contact list

--- AgriculturePresentation/ViewComponents/_DashboardTablePartial.cs
+++ AgriculturePresentation/ViewComponents/_DashboardTablePartial.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgriculturePresentation.ViewComponents
 {
     public class _DashboardTablePartial : ViewComponent
     {
+        private readonly IContactService _contactService;
+
+        public _DashboardTablePartial(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var value = _contactService.GetListAll();
+            return View(value);
         }
     }
 }
