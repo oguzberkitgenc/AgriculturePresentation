8689d22f0057b580cca6c9f90cd9121202543fd2
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-04-05 22:04:50 +0300

    dinamic report

--- AgriculturePresentation/Controllers/ReportController.cs
+++ AgriculturePresentation/Controllers/ReportController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AgriculturePresentation.Models;
+using ClosedXML.Excel;
+using DataAccesLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AgriculturePresentation.Controllers
 {
@@ -12,7 +18,7 @@ namespace AgriculturePresentation.Controllers
 
         public IActionResult StaticReport()
         {
-            ExcelPackage excelPackage= new ExcelPackage();
+            ExcelPackage excelPackage = new ExcelPackage();
             var workBook = excelPackage.Workbook.Worksheets.Add("Dosya 1");
 
             workBook.Cells[1, 1].Value = "Ürün Adı";
@@ -36,7 +42,52 @@ namespace AgriculturePresentation.Controllers
             workBook.Cells[5, 3].Value = "7999 ₺";
 
             var bytes = excelPackage.GetAsByteArray();
-            return File(bytes,"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","YazılımRaporu.xlsx");
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YazılımRaporu.xlsx");
+        }
+        public List<ContactModel> ContactList()
+        {
+            List<ContactModel> contactModels = new List<ContactModel>();
+            using (var context = new AgricultureContext())
+            {
+                contactModels=context.Contects.Select(x=>new ContactModel
+                {
+                    ContactID=x.ContactID,
+                    ContactName=x.Name,
+                    ContactDate=x.Date,
+                    ContactMail=x.Mail,
+                    ContactMessage=x.Message
+                }).ToList();
+            }
+            return contactModels;
+        }
+        public IActionResult ContactReport()
+        {
+            using (var workBook=new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Mesaj Listesi");
+                workSheet.Cell(1, 1).Value = "Mesaj ID";
+                workSheet.Cell(1, 2).Value = "Mesaj Göndere";
+                workSheet.Cell(1, 3).Value = "Mail Adresi";
+                workSheet.Cell(1, 4).Value = "Mesaj İceriği";
+                workSheet.Cell(1, 5).Value = "Mesaj Tarihi";
+
+                int contactRowCount = 2;
+                foreach (var item in ContactList())
+                {
+                    workSheet.Cell(contactRowCount,1).Value = item.ContactID;
+                    workSheet.Cell(contactRowCount,2).Value = item.ContactName;
+                    workSheet.Cell(contactRowCount, 3).Value = item.ContactMail;
+                    workSheet.Cell(contactRowCount,4).Value= item.ContactMessage;
+                    workSheet.Cell(contactRowCount,5).Value= item.ContactDate;
+                    contactRowCount++;
+                }
+                using(var stream=new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content=stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "MesajRapor.xlsx");
+                }
+            }
         }
     }
 }
