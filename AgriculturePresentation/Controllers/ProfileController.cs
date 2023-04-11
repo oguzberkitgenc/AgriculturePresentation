41ae2d4ca47551d5358a529255b310f60161de67
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-04-11 23:42:31 +0300

    ..

--- AgriculturePresentation/Controllers/ProfileController.cs
+++ AgriculturePresentation/Controllers/ProfileController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AgriculturePresentation.Models;
+using DocumentFormat.OpenXml.Office2021.DocumentTasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AgriculturePresentation.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProfileController(UserManager<IdentityUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+            userEditViewModel.Mail = values.Email;
+            userEditViewModel.Phone = values.PhoneNumber;
+            return View(userEditViewModel);
         }
     }
 }
